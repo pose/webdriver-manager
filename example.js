4dff51ba7e73b5e8cb846a0e4e593004422db96e
Alberto Pose <albertopose@gmail.com>
2014-06-29 21:06:08 -0300

    Improving _downloadIfNew by always calling callback.

--- example.js
+++ example.js
@@ -8,6 +8,8 @@ console.log(wm.status());
 // Install Selenium Standalone and Google Chrome
 wm.install(['standalone', 'chrome'], function (err, filenames) {
 
+  console.log(filenames);
+
   // { standalone: 'ready', chrome: 'ready', ie: 'not present' }
   console.log(wm.status());
 
